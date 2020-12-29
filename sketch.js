9a3b5e095bfba6062ae36b91b74c3931b3474e48
Pranjal689 <76397224+Pranjal689@users.noreply.github.com>
2020-12-29 10:00:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function setup() {
 }
 
 function draw() {
-  background("black");  
+  background(255,255,255);  
   drawSprites();
   if(wall.x-car.x<(car.width+wall.width)/2){
     car.velocityX=0;
